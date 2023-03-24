056f754f8a02806f2479ad1733e83bfc14029c26
ikmalsaid <35127268+ikmalsaid@users.noreply.github.com>
2023-03-24 17:23:39 +0800

    Added rocky.py

---
+++ rocky.py
@@ -0,0 +1,15 @@
+import random
+choices = ['rock', 'paper', 'scissors']
+
+def get_cpu_choices():
+    return choices[int(random.randrange(0,3))]
+
+def play_round(player, cpu):
+    if player == cpu:
+        print(f'tie - both choose {player}')
+    elif player == 'rock' and cpu == 'scissors' or player == 'paper' and cpu == 'rock' or player == 'scissors' and cpu == 'paper':
+        print(f'player wins - {player} beats {cpu}')
+    else:
+        print(f'player lose - {cpu} beats {player}')
+    
+play_round(str(input('Rock, paper or scissors? ')), str(get_cpu_choices()))
